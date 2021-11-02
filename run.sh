6aa234dd5c221397690a6b55ca3ea4fde9544186
Paul Woelfel <paul@woelfel.at>
2021-11-02 22:02:59 +0100

    Update startup script and mounted directories

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-set +u
+set -eo pipefail
 
 CONFIG_PATH=/data/options.json
 
 OVPNFILE="$(jq --raw-output '.ovpnfile' $CONFIG_PATH)"
-OPENVPN_CONFIG=/share/${OVPNFILE}
+OPENVPN_CONFIG=${OVPNFILE}
 
 ########################################################################################################################
 # Initialize the tun interface for OpenVPN if not already available
@@ -13,7 +13,7 @@ OPENVPN_CONFIG=/share/${OVPNFILE}
 # Returns:
 #   None
 ########################################################################################################################
-function init_tun_interface(){
+function init_tun_interface() {
     # create the tunnel for the openvpn client
 
     mkdir -p /dev/net
@@ -22,68 +22,13 @@ function init_tun_interface(){
     fi
 }
 
-########################################################################################################################
-# Check if all required files are available.
-# Globals:
-#   REQUIRED_FILES
-#   STORAGE_LOCATION
-# Arguments:
-#   None
-# Returns:
-#   0 if all files are available and 1 otherwise
-########################################################################################################################
-function check_files_available(){
-    failed=0
-
-    if [[ ! -f ${OPENVPN_CONFIG} ]]
-    then
-        echo "File ${OPENVPN_CONFIG} not found"
-        failed=1
-        break
-    fi
-
-    if [[ ${failed} == 0 ]]
-    then
-        return 0
-    else
-        return 1
-    fi
-
-
-}
-
-########################################################################################################################
-# Wait until the user has uploaded all required certificates and keys in order to setup the VPN connection.
-# Globals:
-#   REQUIRED_FILES
-#   CLIENT_CONFIG_LOCATION
-# Arguments:
-#   None
-# Returns:
-#   None
-########################################################################################################################
-function wait_configuration(){
-
-    echo "Wait until the user uploads the files."
-    # therefore, wait until the user upload the required certification files
-    while true; do
-
-        check_files_available
-
-        if [[ $? == 0 ]]
-        then
-            break
-        fi
-
-        sleep 5
-    done
-    echo "All files available!"
-}
+if [[ ! -f ${OPENVPN_CONFIG} ]]; then
+    echo "File ${OPENVPN_CONFIG} not found"
+    echo "Please specify the correct config file path in the settings page"
+    exit 1
+fi
 
 init_tun_interface
-
-# wait until the user uploaded the configuration files
-wait_configuration
 
 echo "Setup the VPN connection with the following OpenVPN configuration."
 
